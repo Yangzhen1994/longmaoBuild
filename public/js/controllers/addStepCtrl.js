3d6050b65890d7ef9147fb0df057dbc8994f61b7
yz <739512172@qq.com>
2017-04-08 14:55:52 +0800

    step tishitupian

--- public/js/controllers/addStepCtrl.js
+++ public/js/controllers/addStepCtrl.js
@@ -210,9 +210,117 @@ define(['app','storageUtils'], function (app,storageUtils) {
                             $( ".selector" ).sortable( "refreshPositions" );*/
 
                         }
-                    };
+        };
                     $scope.step_upload = function (obj) {
-                        alert(obj.parents('ul').eq(0).id.substr(-1,1))
+                        $scope.step_upload = function (obj,event) {
+                            //获取到 步骤index
+                            // event.stopPropagation()
+                            var stepIndex= obj.parentNode.parentNode.parentNode.parentNode.id.substr(-1,1)
+                            var stepImgList = $('#imgUrl'+stepIndex).find('p').eq(0).find('img');
+                            console.log(stepImgList)
+                            for(var i= 0;i<stepImgList.length;i++){
+                                stepImgList[i].setAttribute('id','step'+stepIndex+'Img'+i);
+                                var div = document.createElement('div');
+                                div.id = 'step'+stepIndex+'ImgContainer'+i
+                                $('#imgUrl'+stepIndex).find('p').eq(0).prepend(div);
+                                function sys_file_sdk_qiniu_token(file) {
+                                    var token = $.ajax({
+                                        url: 'http://manager.test.shandianshua.com/sdk/qiniu/token.json',
+                                        method: 'POST',
+                                        xhrFields: {
+                                            withCredentials: true
+                                        },
+                                        data: {bucket: 'totoro-task'},
+                                        dataType: 'json',
+                                        async: false,
+                                    }).responseText;
+
+                                    return jQuery.parseJSON(token).result;
+                                }
+
+                                function showPreview (file) {
+
+                                }
+                                var hashArr = []
+                                var uploader = Qiniu.uploader({
+                                    runtimes: 'html5,html4',
+                                    browse_button: 'step'+stepIndex+'Img'+i,
+                                    container: 'step'+stepIndex+'ImgContainer'+i,
+                                    //drop_element: 'sys-file-dialog-list'+comIndex,
+                                    max_file_size: '100mb',
+                                    //dragdrop: true,
+                                    chunk_size: '5mb',
+                                    multi_selection: true,
+                                    get_new_uptoken: true,
+                                    uptoken_func: sys_file_sdk_qiniu_token,
+                                    domain: ' http://task.totoro.cdn.shandianshua.com',
+                                    unique_names: true,
+                                    auto_start: true,
+                                    init: {
+                                        'FilesAdded': function(up, files) {
+
+                                            /*// 文件添加进队列后，处理相关的事情
+                                             alert('添加了文件');
+                                             console.log(files
+                                             );
+                                             for (var i = 0; i < files.length; i++) {
+
+
+                                             showPreview (files[i]);
+                                             }
+                                             $('#sys-file-dialog-upload-btn'+scope.stepIndex+comIndex).attr("src",'../img/moduleImg/ic_add_a_photo_black_24dp.png');*/
+
+                                        },
+                                        'BeforeUpload': function(up, file) {
+                                            // 每个文件上传前，处理相关的事情
+
+                                            alert('准备上传')
+                                        },
+                                        'UploadProgress': function(up, file) {
+                                            // 每个文件上传时，处理相关的事情
+                                        },
+                                        'FileUploaded': function(up, file, info) {
+                                            // 每个文件上传成功后，处理相关的事情
+                                            // 其中info是文件上传成功后，服务端返回的json，形式如：
+                                            // {
+                                            //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+                                            //    "key": "gogopher.jpg"
+                                            //  }
+                                            // 查看简单反馈
+                                            var domain = up.getOption('domain');
+                                            var res = jQuery.parseJSON(info);
+                                            var str = ''
+                                            for(var i=0;i<hashArr.length;i++){
+                                                str += domain+"/"+hashArr[i].key+'\n'
+                                            }
+                                            console.log(info)
+                                            //var sourceLink = domain +"/"+ res.key+'\n'; //获取上传成功后的文件的Url
+                                            //console.log(sourceLink);
+                                            //scope.stepItems[stepIndex].component[comIndex].tips_image = str
+                                            //console.log(scope.stepItems[stepIndex].component[comIndex]);
+
+
+                                        },
+                                        'Error': function(up, err, errTip) {
+                                            //上传出错时，处理相关的事情
+                                            console.log(err)
+                                        },
+                                        'UploadComplete': function() {
+                                            //队列文件处理完毕后，处理相关的事情
+
+                                        },
+                                        'Key': function(up, file) {
+                                            // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+                                            // 该配置必须要在unique_names: false，save_key: false时才生效
+                                            var key = "";
+                                            // do something with key here
+                                            return key
+                                        }
+                                    }
+                                })
+                            }
+
+                        }
                     }
                     /*上一项 保存任务*/
                     $scope.asprePage = function () {

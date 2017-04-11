8dc2139f94b052f8e7a5dca99ff830f0bb54db2e
yz <739512172@qq.com>
2017-04-11 18:10:03 +0800

    添加 ng-file-upload

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@
     'use strict';
 
     require.config({
+
         waitSeconds: 0,
         //基本路径
         baseUrl: "js/",
@@ -93,7 +94,7 @@
         }
     });
 
-    require(['angular','angular-route','angular-ui-route','ui-sortable','app','route','storageUtils',
+    require(['angular','angular-route','angular-ui-route','ui-sortable','ng-file-upload','app','route','storageUtils',
             'dir_textProof','dir_imgProof','dir_laydate','dir_addStep','dir_posProof','dir_audioProof','dir_showImg','dir_showText','dir_keyEvents','headerCtrl','newTaskCtrl','taskListCtrl',
             'reviewCtrl','reviewDetailCtrl', "toReviewCtrl", "ReviewOkCtrl", "ReviewNoCtrl",
                 "toReviewDetailCtrl",'addStepCtrl','textProofCtrl','imgProofCtrl','posProofCtrl','showImgCtrl','changeLeftNav','serverService','mapService'],

3212a075020c0ee8f32892be54c4b9bc3cf0f87a
yz <739512172@qq.com>
2017-04-11 11:12:10 +0800

    新增shownocheck

--- public/js/controllers/taskListCtrl.js
+++ public/js/controllers/taskListCtrl.js
@@ -219,7 +219,8 @@ define(['app','storageUtils'], function (app,storageUtils) {
                             device:0,
                             user:1,
                             page:1,
-                            rows:200
+                            rows:200,
+                            show_nocheck:1
                         }).then(function (result) {
                             $scope.items = result;
                             console.log($scope.items);

9ec74de3cd73e25de989b466e99cc9f788cb4ec3
yz <739512172@qq.com>
2017-04-12 17:28:19 +0800

    添加图片凭证后，无法传示意图

--- public/js/util/dir_imgProof.js
+++ public/js/util/dir_imgProof.js
@@ -29,7 +29,8 @@ define(['app','storageUtils'], function (app,storageUtils) {
                     //$(this).css('display', 'none')
 
 
-                })
+                });
+
                 el.find('img').eq(-1).click(function (e) {
                     e.stopPropagation();
                     e.stopPropagation();
@@ -78,6 +79,142 @@ define(['app','storageUtils'], function (app,storageUtils) {
                                     //window.location = '#/reviewList';
                                 }
                             });
+                })
+
+
+                /***上传示意图**/
+                el.find('div').eq(2).on('mouseenter',function (e) {
+                    e.stopPropagation();
+                    console.log($(this).parents('li'))
+                    if($(this).parents('li')[0].id.length == 16){
+                        var comIndex = $(this).parents('li')[0].id.substr(-3, 3);
+
+                    }else
+                    if($(this).parents('li')[0].id.length == 15){
+                        var comIndex = $(this).parents('li')[0].id.substr(-2, 2);
+
+                    }else{
+                        //alert($(this).parents('li')[0].id)
+                        var comIndex = $(this).parents('li')[0].id.substr(-1, 1);
+                    }
+
+                    function sys_file_sdk_qiniu_token(file) {
+                        var token = $.ajax({
+                            url: 'http://manager.shandianshua.com/sdk/qiniu/token.json',
+                            method: 'POST',
+                            xhrFields: {
+                                withCredentials: true
+                            },
+                            data: {bucket: 'totoro-task'},
+                            dataType: 'json',
+                            async: false,
+                        }).responseText;
+
+                        return jQuery.parseJSON(token).result;
+                    }
+
+
+                    var hashArr = []
+                    var uploader = Qiniu.uploader({
+                        runtimes: 'html5,html4',
+                        browse_button: 'upimgProof'+scope.stepIndex+comIndex,
+                        container: 'upimgProof-list'+scope.stepIndex+comIndex,
+                        //drop_element: 'sys-file-dialog-list'+comIndex,
+                        max_file_size: '100mb',
+                        //dragdrop: true,
+                        chunk_size: '5mb',
+                        multi_selection: true,
+                        get_new_uptoken: true,
+                        uptoken_func: sys_file_sdk_qiniu_token,
+                        domain: ' http://task.totoro.cdn.shandianshua.com',
+                        unique_names: true,
+                        auto_start: true,
+                        init: {
+                            'FilesAdded': function(up, files) {
+
+                                // 文件添加进队列后，处理相关的事情
+                                alert('添加了文件');
+                                console.log(files
+                                );
+                                for (var i = 0; i < files.length; i++) {
+
+
+                                    showPreview (files[i]);
+                                }
+                                $('#sys-file-dialog-upload-btn'+scope.stepIndex+comIndex).attr("src",'../img/moduleImg/ic_add_a_photo_black_24dp.png');
+
+                            },
+                            'BeforeUpload': function(up, file) {
+                                // 每个文件上传前，处理相关的事情
+
+                                alert('准备上传')
+                            },
+                            'UploadProgress': function(up, file) {
+                                // 每个文件上传时，处理相关的事情
+                            },
+                            'FileUploaded': function(up, file, info) {
+                                console.log(file)
+                                console.log(info);
+
+                                hashArr.push(jQuery.parseJSON(info));
+                                console.log(hashArr);
+                                /*$.ajax({
+                                 url:'http://manager.test.shandianshua.com/sys/file/save.json',
+                                 method:'POST',
+                                 xhrFields: {
+                                 withCredentials: true
+                                 },
+                                 data:{
+                                 space:'',
+                                 key:file.id,
+                                 title:file.name,
+                                 name:file.target_name,
+                                 type:file.type?file.type:file.title.replace(/^[^\.]+\./,''),
+                                 size:file.size
+                                 },
+                                 dataType:'json',
+                                 async:false
+                                 })*/
+                                // 每个文件上传成功后，处理相关的事情
+                                // 其中info是文件上传成功后，服务端返回的json，形式如：
+                                // {
+                                //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+                                //    "key": "gogopher.jpg"
+                                //  }
+                                // 查看简单反馈
+                                var domain = up.getOption('domain');
+                                var res = jQuery.parseJSON(info);
+                                var str = ''
+                                for(var i=0;i<hashArr.length;i++){
+                                    str += domain+"/"+hashArr[i].key+'\n'
+                                }
+                                //var sourceLink = domain +"/"+ res.key+'\n'; //获取上传成功后的文件的Url
+                                //console.log(sourceLink);
+                                scope.stepItems[stepIndex].component[comIndex].tips_image = str
+                                console.log(scope.stepItems[stepIndex].component[comIndex]);
+
+
+                            },
+                            'Error': function(up, err, errTip) {
+                                //上传出错时，处理相关的事情
+                                console.log(err)
+                            },
+                            'UploadComplete': function() {
+                                //队列文件处理完毕后，处理相关的事情
+                                var taskId = storageUtils.session.getItem('_TaskId_') || storageUtils.session.getItem('_newTaskid_')
+                                scope.stepItems[stepIndex].component[comIndex].task_id = taskId
+                                serverService.submitComponent(scope.stepItems[stepIndex].component[comIndex])
+                            },
+                            'Key': function(up, file) {
+                                // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+                                // 该配置必须要在unique_names: false，save_key: false时才生效
+                                var key = "";
+                                // do something with key here
+                                return key
+                            }
+                        }
+                    })
+                    console.log(uploader)
                 })
 
             }

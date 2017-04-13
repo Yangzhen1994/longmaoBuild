bfbe1c7106c04977193446531416b89324265fdb
yz <739512172@qq.com>
2017-04-13 18:16:39 +0800

    新建

--- public/js/controllers/newTaskCtrl.js
+++ public/js/controllers/newTaskCtrl.js
@@ -170,7 +170,7 @@ define(['app','storageUtils'], function (app,storageUtils) {
                 $scope.task.tag_id = '0'
             }
         }
-
+        $scope.changeTag()
         //假设仅仅安卓对应状态 吗 为 0
         $scope.changelineVersion = function () {
             if($scope.lineVersion.version == 'android'){
@@ -185,7 +185,8 @@ define(['app','storageUtils'], function (app,storageUtils) {
                 //alert(1)
                 $scope.task.device = '0'
             }
-        }
+        };
+        $scope.changelineVersion()
         //审核时间转换
         $scope.changeReviewTime = function () {
             $scope.reviewTimeDay = Math.ceil($scope.task.max_check_time*1/24)
@@ -194,7 +195,7 @@ define(['app','storageUtils'], function (app,storageUtils) {
         //保存本页
         $scope.ntsavePage = function () {
             $scope.areaItems.forEach(function (item,index) {
-                if (!$scope.showArea || $socpe.showArea == null) {
+                if (!$scope.showArea || $scope.showArea == null) {
                     if (item.value == $scope.currentArea) {
                         $scope.task.region_filter = item.code;
                     }

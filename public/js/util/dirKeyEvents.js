4f75f19cdbe1da3ae6fdc99d0b5095c901e38ea8
yz <739512172@qq.com>
2017-05-04 16:32:10 +0800

    快捷键审核

--- public/js/util/dirKeyEvents.js
+++ public/js/util/dirKeyEvents.js
@@ -16,12 +16,12 @@ define(['app'], function (app) {
                         var kc = window.event?e.keyCode:e.which;
                         console.log(kc);
                         if(kc == 187 || kc == 107){
-                            $('.tr_Right_fallow').trigger('click')
-                            $('#rn_allow').trigger('click')
+                            $('.tr-right-fallow').trigger('click');
+                            //$('#rnAllow').trigger('click')
                         }else if(kc == 97 || kc == 49){//小键盘1 或者 大键盘1
-                            $('#reson_detail0').trigger('click')
+                            $('#reasonDetail0').trigger('click')
                         }else if(kc == 98 || kc == 50){//小键盘2 或者 大键盘2
-                            $('#reson_detail1').trigger('click')
+                            $('#reasonDetail1').trigger('click')
                         }else if(kc == 99 || kc == 51){
                             scope.showrejCover = true
                             $('#orderReason').trigger('keyup')
